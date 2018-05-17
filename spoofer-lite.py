fb9c83d1f548e04e95d59b0691e60ab028321fef
HD Dananjaya <I2NhbmloZWxweW91@gmail.com>
2018-05-17 20:03:36 +0530

    Add files via upload

---
+++ spoofer-lite.py
@@ -0,0 +1,63 @@
+#
+# spoofer-lite.py
+# -SL4A version for Spoofer.py- 
+# 
+# By 	: @_hddananjaya
+#
+
+
+import smtplib
+
+# define variables
+
+mailServer = ""
+serverPort = 
+email = ""
+password = ""
+
+# print banner
+print (""" 
+-------------------------------
+	spoofer-lite
+-Lite version for Spoofer.py-
+-------------------------------
+""")
+
+
+server = smtplib.SMTP( mailServer, serverPort )
+server.starttls()
+server.login( email, password )
+print ("Server Connected!")
+
+def main():
+
+   # Getting from ,to and message 
+   from_=str(input("\n[$] From(spoofed):"))
+   #to=str(raw_input("[$] To :"))
+   to = ""
+   #to = ""
+   text = str(input("[$] Message:"))
+
+   subject=text
+   message = "Subject: %s\n\n%s" % (subject, text)
+
+
+   # Send mail and dispaly sent..
+   server.sendmail( from_, to, message)
+   print ("Sent!")
+
+
+while __name__ == "__main__":
+   try:
+      main()
+   except KeyboardInterrupt:
+        print ("\nExiting program..")
+
+
+
+
+
+
+
+
+
